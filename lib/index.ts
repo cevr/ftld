caa0b4dc1491303d6ead64b1c3a3f66ffac384da
Cristian Velasquez Ramos <seeve.c@gmail.com>
2023-05-05 18:57:16 -0400

    simplify exports

--- lib/index.ts
+++ lib/index.ts
@@ -1,10 +1,5 @@
-export type { None, Some } from "./option";
-export type { Err, Ok } from "./result";
-export type { Unbrand } from "./brand";
-export type { SettledErr, SettledOk, SettledResult } from "./result";
-export type { TaskSchedulingOptions } from "./task";
-export { Brand } from "./brand";
-export { Result } from "./result";
-export { Option } from "./option";
-export { Task, TaskTimeoutError, TaskSchedulingError } from "./task";
-export { identity, isOption, isResult, isTask } from "./utils";
+export * from "./brand";
+export * from "./option";
+export * from "./result";
+export * from "./task";
+export * from "./utils";
