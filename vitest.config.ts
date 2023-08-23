333c9d5c941bcde36580657adedec71bd7f5301f
Cristian Velasquez Ramos <seeve.c@gmail.com>
2023-08-23 14:30:00 -0400

    alias?

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from "vitest/config";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   test: {
     globals: true,
+    alias: {
+      ftld: path.join(__dirname, "./lib/dist/esm/index.js"),
+    },
   },
 });
